ecd1b0f546f64ca0b0a7276ba61493742ab6b15a
mohi-jpn <lastzephyranthes@yahoo.co.jp>
2021-03-07 01:18:39 +0900

    Refactor: separate options for chart.js

---
+++ docs/js/options.js
@@ -0,0 +1,63 @@
+const options = {
+    title: {
+        display: false,
+    },
+    legend: {
+        display: true,
+    },
+    tooltips: {
+        callbacks: {
+            label: (tooltipItem, data) => {
+                return `score: ${tooltipItem.index + 1}, time: ${tooltipItem.label}`;
+            },
+        },
+    },
+    scales: {
+        xAxes: [{
+            scaleLabel: {
+                display: true,
+                labelString: '時間(秒)',
+            },
+            gridLines: {
+                display: true,
+                color: '#ffffff',
+                zeroLineColor: '#ddd',
+                zeroLineWidth: 2,
+            },
+            ticks: {
+                fontColor: '#333',
+            },
+        }],
+        yAxes: [{
+            scaleLabel: {
+                display: false,
+            },
+            gridLines: {
+                zeroLineColor: '#ddd',
+                zeroLineWidth: 2,
+            },
+            ticks: {
+                display: false,
+                min: -5,
+                max: 5,
+                stepSize: 5,
+            }
+        }]
+    },
+    plugins: {
+        zoom: {
+            pan: {
+                enabled: true,
+                mode: 'x'
+            },
+            zoom: {
+                enabled: true,
+                mode: 'x',
+                sensitivity: 3
+            }
+        },
+        colorschemes: {
+            scheme: 'brewer.Accent8'
+        },
+    }
+};
